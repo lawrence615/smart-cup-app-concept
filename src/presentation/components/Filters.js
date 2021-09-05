15bea31513a6af327e1a1a247bc7f1cd48c37c3b
Lawrence Macharia <macharialawrence2011@gmail.com>
2021-09-06 01:23:03 +0300

    Improving the filters component...now we have the selected category being highlighted

--- src/presentation/components/Filters.js
+++ src/presentation/components/Filters.js
@@ -1,35 +1,34 @@
 import React, { useState } from 'react'
 import { SafeAreaView, View, Text, TouchableOpacity, FlatList, Image, StyleSheet } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import tw from 'tailwind-react-native-classnames'
+
+import { setFilter } from 'reducers/shopSlice'
+import { selectFilter } from 'selectors/shopSlice'
 
 const options = [
   {
-    id: 1,
-    value: "all",
+    id: "all",
     label: "All"
   },
   {
-    id: 2,
-    value: "mugs",
+    id: "mugs",
     label: "Mugs"
   },
   {
-    id: 3,
-    value: "vases",
+    id: "vases",
     label: "Vases"
   },
   {
-    id: 4,
-    value: "plates",
+    id: "plates",
     label: "Plates"
   }
 ]
 
 const Filters = () => {
 
-  const navigation = useNavigation()
+  const dispatch = useDispatch()
   const [selected, setSelected] = useState(null)
 
   return (
@@ -40,16 +39,13 @@ const Filters = () => {
         horizontal
         showsHorizontalScrollIndicator={false}
         renderItem={({ item: { id, value, label }, item }) => (
-          <TouchableOpacity
-            onPress={() => setSelected(value)}
-            style={
-              tw`flex-row justify-between items-center px-3 ${id === selected?.id && 'bg-gray-200'}`}>
-            <View style={tw`m-5 rounded-full`, { width: 80 }}>
-              <TouchableOpacity disabled={!selected} style={tw`py-5 m-3 ${!selected && "bg-gray-300"}`, { backgroundColor: '#d9d9d9', borderRadius: 20 }}>
-                <Text style={tw`text-center p-1 text-base`}>{label}</Text>
-              </TouchableOpacity>
-            </View>
-          </TouchableOpacity>
+          <View style={tw`border-gray-100`, { marginRight: 15, backgroundColor: '#d9d9d9', borderRadius: 20, alignItems: 'center' }}>
+            <TouchableOpacity
+              onPress={() => setSelected(item)}
+              style={tw`rounded-full w-20  ${id === selected?.id && 'bg-black'}`}>
+              <Text style={tw`text-center p-1 text-base ${id === selected?.id && 'text-white '}`}>{label}</Text>
+            </TouchableOpacity>
+          </View>
         )}
       />
     </SafeAreaView>
