1a7a7cd5c3b8554eff76869878d26e6b46d4c8d4
Lawrence Macharia <macharialawrence2011@gmail.com>
2021-09-06 15:55:55 +0300

    new slice, mugSlic

--- src/application/store.js
+++ src/application/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import filterSlice from 'reducers/filterSlice'
+import mugSlice from 'reducers/mugSlice'
 
 export const store = configureStore({
   reducer: {
-    fil: filterSlice
+    fil: filterSlice,
+    mugg: mugSlice
   }
 })
